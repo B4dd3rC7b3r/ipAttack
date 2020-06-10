29d93299afa308f5e23a9450f69fafe229c65ca7
B4dd3rC7b3r <66749087+B4dd3rC7b3r@users.noreply.github.com>
2020-06-11 00:57:03 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+Ip-Attack
+Auto IP or Domain Attack Tool ( #1 )
+
+
+installation
+
+
+heavy_check_mark apt update
+
+heavy_check_mark apt upgrade
+
+heavy_check_mark apt install git python python2
+
+heavy_check_mark cd $HOME
+
+heavy_check_mark git clone https://github.com/B4dd3rC7b3r/Ip-Attack
+
+heavy_check_mark cd Ip-Attack
+
+heavy_check_mark chmod +x requirement.sh ip-attack.py
+
+heavy_check_mark bash requirement.sh
+
+heavy_check_mark python2 ip-attack.py
+
+heavy_check_mark Done...!!
---
+++ requirements.sh
@@ -0,0 +1,30 @@
+#!/bin/python
+# 
+#
+# Name	:	IP Attack
+# Code		:	python
+# Sec.Code	:	B4dd3r
+# Coder		: 	B4dd3rC7b3r
+# Site		: 	B4dd3rCyb3r.blogspot.com
+# github	:	github.com/B4dd3rC7b3r
+#
+#
+#	<==(__WARNING__)==>
+#
+# DO NOT MODIFY OR COPY WITHOUT PERMISSION !!!
+#
+#	<==(__WARNING__)==>
+#
+#
+#
+apt update
+apt upgrade 
+apt install figlet -y
+apt install toilet -y
+apt install nano -y
+apt install ruby -y
+apt install curl -y
+gem install lolcat
+clear
+toilet -f pagga "Done..!!" | lolcat
+© 2020 GitHub, Inc.
